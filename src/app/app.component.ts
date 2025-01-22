beffd09a98a7ef39ebdf4faae0c5291775d52e0d
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-23 00:23:13 +0530

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { ServerComponent } from './dashoard/server/server.component';
+import { TrafficComponent } from './dashoard/traffic/traffic.component';
+import { TicketsComponent } from './dashoard/tickets/tickets.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ServerComponent,TrafficComponent,TicketsComponent],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
-
-}
+}) 
+export class AppComponent {}
+  

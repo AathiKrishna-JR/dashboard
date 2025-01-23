955fcaf5ee0d9b59ba1a1b133879c05d198dd0a0
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-23 11:29:35 +0530

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { ServerComponent } from './dashoard/server/server.component';
 import { TrafficComponent } from './dashoard/traffic/traffic.component';
 import { TicketsComponent } from './dashoard/tickets/tickets.component';
+import { DashboardItemComponent } from './dashoard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ServerComponent,TrafficComponent,TicketsComponent],
+  imports: [HeaderComponent, ServerComponent,TrafficComponent,TicketsComponent,DashboardItemComponent],
   standalone: true,
   templateUrl: './app.component.html',
 }) 

61dd1be768adcf6f9f394b517c6b275d3c59eaa7
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-24 09:57:36 +0530

    form

--- src/app/dashboard/server/server.component.ts
+++ src/app/dashboard/server/server.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html',
   styleUrl: './server.component.css'
 })
-export class ServerComponent {
-  currentStatus= "online";
+export class ServerComponent implements OnInit{
+  currentStatus : 'online' | 'offline' | 'unknown' = "online";
+  
+  constructor(){}
+
+  ngOnInit(): void {
+    
+    setInterval(() => {
+      const rnd = Math.random();
+      if(rnd < 0.5)
+      {
+        this.currentStatus = "offline";
+      }
+      else if(rnd < 0.9){
+        this.currentStatus = "online";
+      }
+      else{
+        this.currentStatus = "unknown";
+      }
+      
+    },5000);
+  }
 }

50560924f1bac0103f0bb7f91d4a1224935b0b36
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-24 12:19:30 +0530

    ticket

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, output, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,11 +11,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent implements OnInit,AfterViewInit{
 
-  
+  add = output<{title : string; text : string}> ();
  // private form = viewChild<ElementRef<HTMLFormElement>>('form')
   @ViewChild('form') form ?: ElementRef<HTMLFormElement>;
   onSubmit(title : string, text : string , form : HTMLFormElement){
-       console.log(title,text);
+        this.add.emit({title : title, text : text})
        this.form?.nativeElement.reset();
   }
 ngOnInit(){
